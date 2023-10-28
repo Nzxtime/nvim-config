c30b200bc40a81bdae327358118918696f2da3cb
Marcel Streicher <marcel.streicher58@gmail.com>
2023-10-28 16:02:51 +0200

    added feature to spawn random animal with @duck.lua

--- lua/Nzxtime/plugins/duck.lua
+++ lua/Nzxtime/plugins/duck.lua
@@ -1,7 +1,13 @@
+function getRandomAnimal()
+    t = {"🦆", "🐕", "🐤", "🐒", "🐻"}
+    return t[math.random(1, #t)]
+end
+
 return {
     'tamton-aquib/duck.nvim',
     config = function()
         vim.keymap.set('n', '<leader>dd', function() require("duck").hatch("🐕", 0.5) end, { desc = 'Spawn Dog' })
+        vim.keymap.set('n', '<leader>dr', function() require("duck").hatch(getRandomAnimal(), 0.5) end, { desc = 'Spawn Random Animal' })
         vim.keymap.set('n', '<leader>dk', function() require("duck").cook() end, { desc = 'Kill Dog' })
     end
 }
